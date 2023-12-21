a6ea1c72570eea7e32438deedee0c09916812936
Md. Shariful Islam <shariful.islam22@ndessl.local>
2023-12-21 16:32:39 +0600

    ProductPriceAndOffer create done

---
+++ NDE Digital Market/DTOs/SellerProductPriceAndOfferDto.cs
@@ -0,0 +1,23 @@
+﻿namespace NDE_Digital_Market.DTOs
+{
+    public class SellerProductPriceAndOfferDto
+    {
+        public int ProductId { get; set; }
+        public int? UserId { get; set; }
+        public decimal? Price { get; set; }
+        public decimal? DiscountAmount { get; set; }
+        public decimal? DiscountPct { get; set; }
+        public DateTime? EffectivateDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public IFormFile? ImageFile { get; set; }
+        //public string? ImagePath { get; set; }
+        //public string? Status { get; set; }
+        public decimal? TotalPrice { get; set; }
+        public string? CompanyCode { get; set; }
+        //public Boolean? IsActive { get; set; }
+        //public DateTime? AddedDate { get; set; }
+        public string? AddedBy { get; set; }
+        public string? AddedPC { get; set; }
+
+    }
+}
