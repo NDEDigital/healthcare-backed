0411d0873e1f3a70c22bb6bbee18d1718956c68e
Md. Shariful Islam <shariful.islam22@ndessl.local>
2023-12-21 20:21:24 +0600

    get order data based on status

---
+++ NDE Digital Market/DTOs/OrderDataBaseOnStatusDto.cs
@@ -0,0 +1,18 @@
+﻿namespace NDE_Digital_Market.DTOs
+{
+    public class OrderDataBaseOnStatusDto
+    {
+        public int? OrderMasterId { get; set; }
+        public string? OrderNo { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public string? Address { get; set; }
+        public int? UserId { get; set; }
+        public string? UserName { get; set; }
+        public string? PaymentMethod { get; set; }
+        public int? NumberOfItem { get; set; }
+        public decimal? TotalPrice { get; set; }
+        public string? PhoneNumber { get; set; }
+        public decimal? DeliveryCharge { get; set; }
+        public string? Status { get; set; }
+    }
+}
