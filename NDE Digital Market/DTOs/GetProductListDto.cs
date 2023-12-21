8652b62b6d4c076ddc76477df3e10ade5fee380e
Md. Shariful Islam <shariful.islam22@ndessl.local>
2023-12-21 17:12:04 +0600

    product list get done

---
+++ NDE Digital Market/DTOs/GetProductListDto.cs
@@ -0,0 +1,8 @@
+﻿namespace NDE_Digital_Market.DTOs
+{
+    public class GetProductListDto
+    {
+        public int? ProductId { get; set; }
+        public string? ProductName { get; set; }
+    }
+}
