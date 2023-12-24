542ad8d148275778a3c8d1973642191095e3f789
Helena Akter Reya <helena.aktar@ndessl.local>
2023-12-24 12:50:29 +0600

    searched data get

--- NDE Digital Market/Controllers/ProductSearchController.cs
+++ NDE Digital Market/Controllers/ProductSearchController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using NDE_Digital_Market.DTOs;
 using NDE_Digital_Market.Model;
 using System.Data;
 using System.Data.SqlClient;
@@ -11,35 +13,38 @@ namespace NDE_Digital_Market.Controllers
     [ApiController]
     public class ProductSearchController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
         private readonly string connectionDatabase;
+        private readonly string _healthCareConnection;
 
         public ProductSearchController(IConfiguration config)
         {
+            _configuration = config;
+
             connectionDatabase = config.GetConnectionString("DigitalMarketConnection");
+            _healthCareConnection = _configuration.GetConnectionString("HealthCare");
 
         }
 
-        [HttpGet("search")]
-        public IActionResult GetSearchProduct(string SearchKeyword, int Offset, int NextCount, string SortDirection)
+        [HttpGet("GetSearchedProduct")]
+        public IActionResult GetSearchedProduct(string productName, string sortDirection, int nextCount, int offset)
         {
-
-            if (Offset != 0)
+            if (offset != 0)
             {
-                Offset = 20 * (Offset - 1);
+                offset = 20 * (offset - 1);
             }
 
-            var resultList = new List<ProductSearch>();
+            var resultList = new List<ProductSearchDto>();
 
-            using (SqlConnection connection = new SqlConnection(connectionDatabase))
+            using (SqlConnection connection = new SqlConnection(_healthCareConnection))
             {
-                using (SqlCommand cmd = new SqlCommand("sp_SearchProduct", connection))
+                using (SqlCommand cmd = new SqlCommand("ProductSearch", connection))
                 {
                     cmd.CommandType = CommandType.StoredProcedure;
-
-                    cmd.Parameters.Add(new SqlParameter("@SortDirection", SortDirection));
-                    cmd.Parameters.Add(new SqlParameter("@NextCount", NextCount));
-                    cmd.Parameters.Add(new SqlParameter("@Offset", Offset));
-                    cmd.Parameters.Add(new SqlParameter("@SearchKeyword", SearchKeyword));
+                    cmd.Parameters.Add(new SqlParameter("@ProductName", productName));
+                    cmd.Parameters.Add(new SqlParameter("@SortDirection", sortDirection));
+                    cmd.Parameters.Add(new SqlParameter("@NextCount", nextCount));
+                    cmd.Parameters.Add(new SqlParameter("@Offset", offset));
 
                     connection.Open();
 
@@ -47,31 +52,39 @@ namespace NDE_Digital_Market.Controllers
                     {
                         while (reader.Read())
                         {
-                            resultList.Add(new ProductSearch
-                            {
-                                GoodsName = reader["GoodsName"].ToString(),
-                                GroupCode = reader["GroupCode"].ToString(),
-                                GroupName = reader["GroupName"].ToString(),
-                                GoodsId = reader.GetInt32(reader.GetOrdinal("GoodsId")),
-                                Specification = reader["Specification"].ToString(),
-                                ApproveSalesQty = float.Parse(reader["Quantity"].ToString()),
-                                Price = decimal.Parse(reader["Price"].ToString()),
-                                ImagePath = reader["ImagePath"].ToString(),
-                                CompanyName = reader["CompanyName"].ToString(),
-                                TotalCount = int.Parse(reader["TotalCount"].ToString()),
-                                SellerCode = reader["SellerCode"].ToString()
-                            });
+
+                                var productSearchDto = new ProductSearchDto();
+
+                                productSearchDto.CompanyName = reader["CompanyName"] is DBNull ? null : (string)reader["CompanyName"];
+                                productSearchDto.ProductId = reader["ProductId"] is DBNull ? (int?)null : (int)reader["ProductId"];
+                                productSearchDto.ProductName = reader["ProductName"] is DBNull ? null : (string)reader["ProductName"];
+                                productSearchDto.ProductGroupID = reader["ProductGroupID"] is DBNull ? (int?)null : (int)reader["ProductGroupID"];
+                                productSearchDto.ProductGroupName = reader["ProductGroupName"] is DBNull ? null : (string)reader["ProductGroupName"];
+                                productSearchDto.Specification = reader["Specification"] is DBNull ? null : (string)reader["Specification"];
+                                productSearchDto.UnitId = reader["UnitId"] is DBNull ? (int?)null : (int)reader["UnitId"];
+                                productSearchDto.Unit = reader["Unit"] is DBNull ? null : (string)reader["Unit"];
+                                productSearchDto.Price = reader["Price"] is DBNull ? (decimal?)null : (decimal)reader["Price"];
+                                productSearchDto.DiscountAmount = reader["DiscountAmount"] is DBNull ? (decimal?)null : (decimal)reader["DiscountAmount"];
+                                productSearchDto.DiscountPct = reader["DiscountPct"] is DBNull ? (decimal?)null : (decimal)reader["DiscountPct"];
+                                productSearchDto.ImagePath = reader["ImagePath"] is DBNull ? null : (string)reader["ImagePath"];
+                                productSearchDto.TotalPrice = reader["TotalPrice"] is DBNull ? (decimal?)null : (decimal)reader["TotalPrice"];
+                                productSearchDto.SellerId = reader["SellerId"] is DBNull ? (int?)null : (int)reader["SellerId"];
+                                productSearchDto.AvailableQty = reader["AvailableQty"] is DBNull ? (decimal?)null : (decimal)reader["AvailableQty"];
+                                productSearchDto.TotalCount = reader["TotalCount"] is DBNull ? (int?)null : (int)reader["TotalCount"];
+
+                                resultList.Add(productSearchDto);
+
                         }
                     }
                 }
             }
 
             return Ok(resultList);
-
         }
 
-    }
 
     }
+
+}
 
 
---
+++ NDE Digital Market/DTOs/ProductSearchDto.cs
@@ -0,0 +1,22 @@
+﻿namespace NDE_Digital_Market.DTOs
+{
+    public class ProductSearchDto
+    {
+        public string? CompanyName { get; set; }
+        public int? ProductId { get; set; }
+        public string? ProductName { get; set; }
+        public int? ProductGroupID { get; set; }
+        public string? ProductGroupName { get; set; }
+        public string? Specification { get; set; }
+        public int? UnitId { get; set; }
+        public string? Unit { get; set; }
+        public decimal? Price { get; set; }
+        public decimal? DiscountAmount { get; set; }
+        public decimal? DiscountPct { get; set; }
+        public string? ImagePath { get; set; }
+        public decimal? TotalPrice { get; set; }
+        public int? SellerId { get; set; }
+        public decimal? AvailableQty { get; set; }
+        public int? TotalCount { get; set; }
+    }
+}
