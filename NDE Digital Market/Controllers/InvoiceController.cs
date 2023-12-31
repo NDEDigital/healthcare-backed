6e1b2d51e8ecbd26795f629968c01df2bcff5b00
Abdul Latif <a.latif@ndessl.local>
2023-12-31 19:00:29 +0600

    Invoice Controller Updated

--- NDE Digital Market/Controllers/InvoiceController.cs
+++ NDE Digital Market/Controllers/InvoiceController.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 using System.Data;
 using Microsoft.AspNetCore.Authorization;
 using NDE_Digital_Market.DTOs;
+using NDE_Digital_Market.Model;
 
 namespace NDE_Digital_Market.Controllers
 {
@@ -30,7 +31,7 @@ namespace NDE_Digital_Market.Controllers
         // Get Invoice data For Admin
         //[HttpGet, Authorize(Roles = "admin")]
         [HttpGet]
-        [Route("GetInvoiceDataForAdmin")]
+        [Route("GetInvoiceDataForBuyer")]
         public async Task<IActionResult> GetInvoiceDataForAdminAsync(int OrderMasterId)
         {
             try
@@ -158,67 +159,148 @@ namespace NDE_Digital_Market.Controllers
         //    return Ok(new { message = "Sellers Order Invoice got successfully", invoice });
         //}
 
+
         //========================================== Added By Rey =================================
         // Get Invoice data For Seller
 
-
-        [HttpGet, Authorize(Roles = "seller")]
+        [HttpGet]
         [Route("GetInvoiceDataForSeller")]
-        public IActionResult GetInvoiceDataForSeller(string sellerCode, int OrderID)
+        public async Task<IActionResult> GetInvoiceDataForSeller(int SSMId)
         {
-            string decryptedSupplierCode = CommonServices.DecryptPassword(sellerCode);
-            SellerInvoiceModel invoice = new SellerInvoiceModel();
-            SqlConnection con = new SqlConnection(_connectionDigitalMarket);
-            string queryForSeller = "sp_InvoiceDataForSeller";
-            con.Open();
-            SqlCommand cmdForSeller = new SqlCommand(queryForSeller, con);
-            cmdForSeller.CommandType = CommandType.StoredProcedure;
-            cmdForSeller.Parameters.AddWithValue("@SellerCode", decryptedSupplierCode);
-            cmdForSeller.Parameters.AddWithValue("@OrderID", OrderID);
-            SqlDataAdapter adapter = new SqlDataAdapter(cmdForSeller);
-            DataSet ds = new DataSet();
-            adapter.Fill(ds);
-            DataTable dt = ds.Tables[0];
-            DataTable dt1 = ds.Tables[1];
-            DataTable dt2 = ds.Tables[2];
-            con.Close();
-            for (int i = 0; i < dt.Rows.Count; i++)
+            try
             {
-                invoice.InvoiceNumber = dt.Rows[i]["InvoiceNumber"].ToString();
-                invoice.GenerateDate = dt.Rows[i]["generateDate"].ToString();
-                invoice.DeliveryDate = dt.Rows[i]["generateDate"].ToString();
-                invoice.TotalPrice = Convert.ToSingle(dt.Rows[i]["TotalPrice"]);
-                invoice.SellerName = dt.Rows[i]["SellerName"].ToString();
-                invoice.SellerCompanyName = dt.Rows[i]["SellerCompanyName"].ToString();
-                invoice.SellerAddress = dt.Rows[i]["SellerAddress"].ToString();
-                invoice.SellerPhone = dt.Rows[i]["SellerPhone"].ToString();
-            }
-            for (int i = 0; i < dt1.Rows.Count; i++)
-            {
-                invoice.OrderNo = dt1.Rows[i]["OrderNo"].ToString();
-                invoice.OrderDate = dt1.Rows[i]["OrderDate"].ToString();
-                invoice.BuyerName = dt1.Rows[i]["BuyerName"].ToString();
-                invoice.BuyerAddress = dt1.Rows[i]["BuyerAddress"].ToString();
-                invoice.BuyerPhone = dt1.Rows[i]["BuyerPhone"].ToString();
-            }
-            for (int i = 0; i < dt2.Rows.Count; i++)
-            {
-                ProductDetails product = new ProductDetails
+                List<SellerInvoice> objectlist = new List<SellerInvoice>();
+
+                using (SqlConnection con = new SqlConnection(connectionHealthCare))
                 {
-                    ProductName = dt2.Rows[i]["ProductName"].ToString(),
-                   
-                    Specification = dt2.Rows[i]["Specification"].ToString(),
-                    Quantity = Convert.ToInt32(dt2.Rows[i]["Quantity"]),
-                    Price = Convert.ToSingle(dt2.Rows[i]["Price"]),
-                    Discount = Convert.ToSingle(dt2.Rows[i]["Discount"]),
-                    DeliveryCharge = Convert.ToSingle(dt2.Rows[i]["DeliveryCharge"]),
-                    SubTotalPrice = Convert.ToSingle(dt2.Rows[i]["SubTotalPrice"])
-                };
-                // Add the ProductDetails object to the productDetailsList
-                invoice.ProductDetailsList.Add(product);
+                    string query = "SellerInvoice";
+
+                    SqlCommand sqlCommand = new SqlCommand(query, con);
+
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.Parameters.AddWithValue("@SSMId", SSMId);
+
+                    await con.OpenAsync();
+
+                    SqlDataReader reader = await sqlCommand.ExecuteReaderAsync();
+
+                    if (!reader.HasRows)
+                    {
+                        return BadRequest(new { message = "No Order Data Found." });
+                    }
+
+                    while (await reader.ReadAsync())
+                    {
+                        SellerInvoice details = new SellerInvoice();
+                        {
+                            details.SSMId = Convert.ToInt32(reader["SSMId"].ToString());
+                            details.SSMCode = reader["SSMCode"].ToString();
+                            details.SSMDate = Convert.ToDateTime(reader["SSMDate"].ToString());
+                            details.CompanyCode = reader["CompanyCode"].ToString();
+                            details.CompanyName = reader["CompanyName"].ToString();
+                            details.TotalPrice = Convert.ToDecimal(reader["TotalPrice"].ToString());
+                            details.Challan = reader["Challan"].ToString();
+                            details.TotalPrice = Convert.ToDecimal(reader["TotalPrice"].ToString());
+                            details.Remarks = reader["Remarks"].ToString();
+                            details.BUserId = Convert.ToInt32(reader["BUserId"].ToString());
+                            details.BuyerName = reader["BuyerName"].ToString();
+                            details.OrderNo = reader["OrderNo"] == DBNull.Value ? null : reader["OrderNo"].ToString();
+                            details.ProductId = Convert.ToInt32(reader["ProductId"].ToString());
+                            details.ProductName = reader["ProductName"].ToString();
+                            details.Specification = reader["Specification"].ToString();
+
+
+                            details.StockQty = Convert.ToDecimal(reader["StockQty"].ToString());
+                            details.SaleQty = Convert.ToInt32(reader["SaleQty"].ToString());
+
+                            details.UnitId = Convert.ToInt32(reader["UnitId"].ToString());
+                            details.Unit = reader["Unit"].ToString();
+
+
+                            details.NetPrice = Convert.ToDecimal(reader["NetPrice"].ToString());
+
+
+                            details.SSLRemarks = reader["SSLRemarks"].ToString();
+
+
+                            details.Address = reader["Address"].ToString();
+
+                            details.ProductGroupID = Convert.ToInt32(reader["ProductGroupID"].ToString());
+                            details.ProductGroupName = reader["ProductGroupName"].ToString();
+                        }
+
+                        objectlist.Add(details);
+                    }
+                    await con.CloseAsync();
+                }
+
+                return Ok(objectlist);
             }
-            return Ok(new { message = "Sellers Order Invoice got successfully", invoice });
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
         }
+
+
+        /////==============================================================================================
+        //[HttpGet, Authorize(Roles = "seller")]
+        //[Route("GetInvoiceDataForSeller")]
+        //public IActionResult GetInvoiceDataForSeller(string sellerCode, int OrderID)
+        //{
+        //    string decryptedSupplierCode = CommonServices.DecryptPassword(sellerCode);
+        //    SellerInvoiceModel invoice = new SellerInvoiceModel();
+        //    SqlConnection con = new SqlConnection(_connectionDigitalMarket);
+        //    string queryForSeller = "sp_InvoiceDataForSeller";
+        //    con.Open();
+        //    SqlCommand cmdForSeller = new SqlCommand(queryForSeller, con);
+        //    cmdForSeller.CommandType = CommandType.StoredProcedure;
+        //    cmdForSeller.Parameters.AddWithValue("@SellerCode", decryptedSupplierCode);
+        //    cmdForSeller.Parameters.AddWithValue("@OrderID", OrderID);
+        //    SqlDataAdapter adapter = new SqlDataAdapter(cmdForSeller);
+        //    DataSet ds = new DataSet();
+        //    adapter.Fill(ds);
+        //    DataTable dt = ds.Tables[0];
+        //    DataTable dt1 = ds.Tables[1];
+        //    DataTable dt2 = ds.Tables[2];
+        //    con.Close();
+        //    for (int i = 0; i < dt.Rows.Count; i++)
+        //    {
+        //        invoice.InvoiceNumber = dt.Rows[i]["InvoiceNumber"].ToString();
+        //        invoice.GenerateDate = dt.Rows[i]["generateDate"].ToString();
+        //        invoice.DeliveryDate = dt.Rows[i]["generateDate"].ToString();
+        //        invoice.TotalPrice = Convert.ToSingle(dt.Rows[i]["TotalPrice"]);
+        //        invoice.SellerName = dt.Rows[i]["SellerName"].ToString();
+        //        invoice.SellerCompanyName = dt.Rows[i]["SellerCompanyName"].ToString();
+        //        invoice.SellerAddress = dt.Rows[i]["SellerAddress"].ToString();
+        //        invoice.SellerPhone = dt.Rows[i]["SellerPhone"].ToString();
+        //    }
+        //    for (int i = 0; i < dt1.Rows.Count; i++)
+        //    {
+        //        invoice.OrderNo = dt1.Rows[i]["OrderNo"].ToString();
+        //        invoice.OrderDate = dt1.Rows[i]["OrderDate"].ToString();
+        //        invoice.BuyerName = dt1.Rows[i]["BuyerName"].ToString();
+        //        invoice.BuyerAddress = dt1.Rows[i]["BuyerAddress"].ToString();
+        //        invoice.BuyerPhone = dt1.Rows[i]["BuyerPhone"].ToString();
+        //    }
+        //    for (int i = 0; i < dt2.Rows.Count; i++)
+        //    {
+        //        ProductDetails product = new ProductDetails
+        //        {
+        //            ProductName = dt2.Rows[i]["ProductName"].ToString(),
+                   
+        //            Specification = dt2.Rows[i]["Specification"].ToString(),
+        //            Quantity = Convert.ToInt32(dt2.Rows[i]["Quantity"]),
+        //            Price = Convert.ToSingle(dt2.Rows[i]["Price"]),
+        //            Discount = Convert.ToSingle(dt2.Rows[i]["Discount"]),
+        //            DeliveryCharge = Convert.ToSingle(dt2.Rows[i]["DeliveryCharge"]),
+        //            SubTotalPrice = Convert.ToSingle(dt2.Rows[i]["SubTotalPrice"])
+        //        };
+        //        // Add the ProductDetails object to the productDetailsList
+        //        invoice.ProductDetailsList.Add(product);
+        //    }
+        //    return Ok(new { message = "Sellers Order Invoice got successfully", invoice });
+        //}
 
 
 
---
+++ NDE Digital Market/Model/SellerInvoice.cs
@@ -0,0 +1,29 @@
+﻿namespace NDE_Digital_Market.Model
+{
+    public class SellerInvoice
+    {
+        public int SSMId { get; set; }
+        public string SSMCode { get; set; }
+        public DateTime SSMDate { get; set; }
+        public string CompanyCode { get; set; }
+        public string CompanyName { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string Challan { get; set; }
+        public string Remarks { get; set; }
+        public int BUserId { get; set; }
+        public string BuyerName { get; set; }
+        public string OrderNo { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Specification { get; set; }
+        public decimal StockQty { get; set; }
+        public int SaleQty { get; set; }
+        public int UnitId { get; set; }
+        public string Unit { get; set; }
+        public decimal NetPrice { get; set; }
+        public string SSLRemarks { get; set; }
+        public string Address { get; set; }
+        public int ProductGroupID { get; set; }
+        public string ProductGroupName { get; set; }
+    }
+}
