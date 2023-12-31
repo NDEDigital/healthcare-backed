2a097f7445c3be30abd2570b6bba123d139b9dec
Md. Shariful Islam <shariful.islam22@ndessl.local>
2023-12-31 16:58:23 +0600

    new model added for returntype

---
+++ NDE Digital Market/Model/ReturnTypeModel.cs
@@ -0,0 +1,8 @@
+﻿namespace NDE_Digital_Market.Model
+{
+    public class ReturnTypeModel
+    {
+        public int? ReturnTypeId { get; set; }
+        public string? ReturnTypeName { get; set; }
+    }
+}
