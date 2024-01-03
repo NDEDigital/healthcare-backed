516b28d36fb900dd01e09ba0cb05b1ae4f2738fd
Md. Shariful Islam <shariful.islam22@ndessl.local>
2024-01-03 19:01:41 +0600

    working on buyer order get

---
+++ NDE Digital Market/DTOs/OrderDetails2ndMasterForBuyerDto.cs
@@ -0,0 +1,19 @@
+﻿namespace NDE_Digital_Market.DTOs
+{
+    public class OrderDetails2ndMasterForBuyerDto
+    {
+        public int? SellerId { get; set; }
+        public DateTime? DeliveryDate { get; set; }
+        public string? Status { get; set; }
+        public decimal? PackageSubtotal { get; set; } = decimal.Zero;
+        public int? PackageDeliveryCharge { get; set; } = 0;
+
+        public List<OrderDetails2ndMDetailsForBuyerDto>? OrderDetails2ndMDetailsListForBuyer { get; set; }
+
+        public OrderDetails2ndMasterForBuyerDto()
+        {
+            OrderDetails2ndMDetailsListForBuyer = new List<OrderDetails2ndMDetailsForBuyerDto>();
+        }
+
+    }
+}
